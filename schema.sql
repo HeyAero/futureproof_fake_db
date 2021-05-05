83fbcf8ed3a323e2a033e98a6d834d8fd6c0fd8a
Roselyn Le <roselynle95@gmail.com>
2021-05-05 17:10:11 +0100

    fix typo

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS people;
 CREATE TABLE people (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name varchar(255) NOT NULL,
-  teacher varchat(255) NOT NULL
+  teacher varchar(255) NOT NULL
 );
 
 INSERT INTO people (name, teacher) VALUES ("Beth", "True"), ("Aaron", "False"), ("Adil", "False"), ( "Roselyn", "False")
