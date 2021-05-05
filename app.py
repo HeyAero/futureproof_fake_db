679a6c0336bd904001f8da58350cb077cb7d7adb
HeyAero <aeroyts@icloud.com>
2021-05-05 14:33:27 +0100

    add database with sqlite3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, g
 from flask_cors import CORS
 from controllers import people
 from werkzeug import exceptions
@@ -7,8 +7,11 @@ import sqlite3
 app = Flask(__name__)
 CORS(app)
 
+DATABASE = "./database/database.db"
+
 @app.route('/')
 def home():
+    init_db()
     return jsonify({'message': 'Hello from futureproof DB!'}), 200
 
 @app.route('/people', methods=['GET', 'POST'])
@@ -19,6 +22,26 @@ def people_handler():
   }
   resp, code = fns[request.method](request)
   return jsonify(resp), code
+
+def get_db():
+  db = getattr(g, '_database', None)
+  if db is None:
+    db = g._database = sqlite3.connect(DATABASE)
+  return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+  db = getattr(g, '_database', None)
+  if db is not None:
+    db.close()
+
+
+def init_db():
+  with app.app_context():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+      db.cursor().executescript(f.read())
+    db.commit()
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS people;
+
+CREATE TABLE people (
+  id INT NOT NULL PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  teacher varchat(255) NOT NULL
+);
